1cb3ddf75e8129687572c10e22d5dbf7b621b820
Toni Ruottu <toni.ruottu@maas.global>
2019-09-18 17:32:14 +0300

    foobers

--- src/converter.ts
+++ src/converter.ts
@@ -1,37 +1,25 @@
-#!/usr/bin/env ts-node -T
+#!/usr/bin/env ts-node
 
 import fs from 'fs';
 import path from 'path';
 import * as gen from 'io-ts-codegen';
 
-const [_tsNode, _script, inputFile, outputDir] = process.argv;
+const [, , inputFile, outputDir] = process.argv;
+
+console.error(`yarn convert ${path.resolve(inputFile)}`);
 
 const schema: JSONSchemaFile = JSON.parse(fs.readFileSync(inputFile, 'utf-8'));
-
-const outputFile = path.join(outputDir, inputFile.split('.json').join('.ts'));
-
-fs.mkdirSync(path.dirname(outputFile), { recursive: true });
-const fd = fs.openSync(outputFile, 'w');
-fs.writeFileSync(fd, '');
-
-const log = (a: string) => fs.appendFileSync(fd, `${a}\n`);
-
 
 export interface StringSchema {
   description?: string;
   pattern?: string;
   minLength?: number;
   maxLength?: number;
-  minimum?: number;
-  maximum?: number;
   type: 'string';
 }
 
 export interface NumberSchema {
   description?: string;
-  pattern?: string;
-  minLength?: number;
-  maxLength?: number;
   minimum?: number;
   maximum?: number;
   type: 'number' | 'integer';
@@ -39,21 +27,11 @@ export interface NumberSchema {
 
 export interface BooleanSchema {
   description?: string;
-  pattern?: string;
-  minLength?: number;
-  maxLength?: number;
-  minimum?: number;
-  maximum?: number;
   type: 'boolean';
 }
 
 export interface ObjectSchema {
   description?: string;
-  pattern?: string;
-  minLength?: number;
-  maxLength?: number;
-  minimum?: number;
-  maximum?: number;
   type: 'object';
   properties: { [key: string]: JSONSchema };
   required?: Array<string>;
@@ -96,27 +74,6 @@ function toInterfaceCombinator(schema: ObjectSchema): gen.InterfaceCombinator {
   );
 }
 
-export function fromSchema(schema: JSONSchema): gen.TypeReference {
-  switch (schema.type) {
-    case 'string':
-      return gen.stringType;
-    case 'number':
-      return gen.numberType;
-    case 'integer':
-      return gen.numberType;
-    case 'boolean':
-      return gen.booleanType;
-    case 'object':
-      return toInterfaceCombinator(schema);
-  }
-}
-
-function capitalize(word: string) {
-  const empty: '' = '';
-  const [c, ...cs] = word.split(empty);
-  return [c.toUpperCase(), ...cs].join(empty);
-}
-
 function checkPattern(x: string, pattern: string): string {
   return `(${x}.match(/${pattern}/) !== null)`;
 }
@@ -141,22 +98,95 @@ function checkInteger(x: string): string {
   return `(Number.isInteger(${x}))`;
 }
 
-function generateChecks(x: string, schema: JSONSchema): string {
-  const checks = [
-    `${x} === ${x}`,
+function stringChecks(x: string, schema: StringSchema): string {
+  const checks: Array<string> = [
     ...(schema.pattern ? [checkPattern(x, schema.pattern)] : []),
     ...(schema.minLength ? [checkMinLength(x, schema.minLength)] : []),
     ...(schema.maxLength ? [checkMaxLength(x, schema.maxLength)] : []),
+  ];
+  if (checks.length < 1) {
+    return 'true';
+  }
+  return checks.join(' && ');
+}
+
+function numberChecks(x: string, schema: NumberSchema): string {
+  const checks: Array<string> = [
+    ...(schema.minimum ? [checkMinimum(x, schema.minimum)] : []),
+    ...(schema.maximum ? [checkMaximum(x, schema.maximum)] : []),
+  ];
+  if (checks.length < 1) {
+    return 'true';
+  }
+  return checks.join(' && ');
+}
+
+function intChecks(x: string, schema: NumberSchema): string {
+  const checks: Array<string> = [
     ...(schema.minimum ? [checkMinimum(x, schema.minimum)] : []),
     ...(schema.maximum ? [checkMaximum(x, schema.maximum)] : []),
     ...(schema.type === 'integer' ? [checkInteger(x)] : []),
   ];
+  if (checks.length < 1) {
+    return 'true';
+  }
   return checks.join(' && ');
 }
 
+function boolChecks(_x: string, _schema: BooleanSchema): string {
+  const checks: Array<string> = [];
+  if (checks.length < 1) {
+    return 'true';
+  }
+  return checks.join(' && ');
+}
+
+export function fromSchema(schema: JSONSchema): gen.TypeReference {
+  switch (schema.type) {
+    case 'string':
+      return gen.stringType;
+    case 'number':
+      return gen.numberType;
+    case 'integer':
+      return gen.numberType;
+    case 'boolean':
+      return gen.booleanType;
+    case 'object':
+      return toInterfaceCombinator(schema);
+  }
+}
+
+export function branded(name: string, schema: JSONSchema): gen.TypeReference {
+  switch (schema.type) {
+    case 'string':
+      return gen.brandCombinator(
+        fromSchema(schema),
+        (x) => stringChecks(x, schema),
+        name,
+      );
+    case 'number':
+      return gen.brandCombinator(
+        fromSchema(schema),
+        (x) => numberChecks(x, schema),
+        name,
+      );
+    case 'integer':
+      return gen.brandCombinator(fromSchema(schema), (x) => intChecks(x, schema), name);
+    case 'boolean':
+      return gen.brandCombinator(fromSchema(schema), (x) => boolChecks(x, schema), name);
+    case 'object':
+      return fromSchema(schema);
+  }
+}
+
+function capitalize(word: string) {
+  const empty: '' = '';
+  const [c, ...cs] = word.split(empty);
+  return [c.toUpperCase(), ...cs].join(empty);
+}
 export function fromFile(
   schema: JSONSchemaFile,
-): Array<[string | undefined, gen.TypeDeclaration]> {
+): Array<[string | undefined, gen.TypeDeclaration, Array<string>]> {
   /*
   const path = schema.$id;
   const [noSuffix] = path.split('.json');
@@ -179,7 +209,6 @@ export function fromFile(
       const [, bobobo] = URI.split('http://maasglobal.com/');
       const [bobobo2] = bobobo.split('#');
       const [bobobo3] = bobobo2.split('.json');
-      log(`import * as ${iname} from 'src/${bobobo3}';`);
       return [
         ref.description,
         gen.typeDeclaration(
@@ -187,34 +216,38 @@ export function fromFile(
           gen.customCombinator(omg, omg, ['foo', 'bar']),
           true,
         ),
+        [`import * as ${iname} from 'src/${bobobo3}';`]
       ];
     }
-    return [
-      scem.description,
-      gen.typeDeclaration(
-        capitalize(k),
-        gen.brandCombinator(
-          fromSchema(scem),
-          (x) => generateChecks(x, scem),
-          `${capitalize(k)}`,
-        ),
-        true,
-      ),
-    ];
+    const name = capitalize(k);
+    return [scem.description, gen.typeDeclaration(name, branded(name, scem), true), [`import * as t from 'io-ts';`]];
   });
 }
 
+const declarations = fromFile(schema as JSONSchemaFile);
+
+const outputFile = path.join(outputDir, inputFile.split('.json').join('.ts'));
+console.log(outputFile)
+
+fs.mkdirSync(path.dirname(outputFile), { recursive: true });
+const fd = fs.openSync(outputFile, 'w');
+fs.writeFileSync(fd, '');
+
+const log = (a: string) => fs.appendFileSync(fd, `${a}\n`);
+
 log('/*');
 log('');
-log(schema.description);
+log(`${schema.description}`);
 log('');
 log('!!! AUTO GENERATED BY CONVERT.TS REFRAIN FROM MANUAL EDITING !!!');
 log('');
 log('*/');
 log('');
-log("import * as t from 'io-ts';");
-
-const declarations = fromFile(schema as JSONSchemaFile);
+const imps = new Set<string>();
+for (const [,, imp] of declarations) {
+  imp.forEach((i) => imps.add(i));
+}
+imps.forEach(log);
 log('');
 log(`export const schemaId = '${schema.$id}';`);
 
@@ -226,7 +259,8 @@ for (const [c, d] of declarations) {
     log(`// ${c}`);
   }
   log(gen.printStatic(d));
-  log(gen.printRuntime(d));
+  log(gen.printRuntime(d).replace(/\ninterface /, '\nexport interface '));
 }
 
+log('// Success');
 fs.closeSync(fd);
