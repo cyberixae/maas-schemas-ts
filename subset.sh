8542861e79bda5f4131c0dc454d5c3395e29d339
Toni Ruottu <toni.ruottu@maas.global>
2019-09-25 13:14:43 +0300

    subset

---
+++ subset.sh
@@ -0,0 +1,13 @@
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/leg.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/modes/MODE_TAXI.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/customer.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/components/state.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/components/fare.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/components/state-log.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/components/common.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/components/travel-mode.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/components/units-geo.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/components/units.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/components/address.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/components/terms.json" src
+./converter.ts "node_modules/@maasglobal/maas-schemas/schemas/core/booking.json" src
