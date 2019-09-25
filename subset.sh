063da7c5e3004c5f40717a8ce2a0a993d9bde841
Toni Ruottu <toni.ruottu@maas.global>
2019-09-25 14:07:29 +0300

    subfiz

--- subset.sh
+++ subset.sh
@@ -1,15 +1,15 @@
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/leg.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/modes
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/modes/mode-taxi.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/customer.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components/state.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components/fare.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components/state-log.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components/common.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components/travel-mode.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components/units-geo.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components/units.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components/address.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/components/terms.json src
-./converter.ts node_modules/\@maasglobal/maas-schemas/core/booking.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/leg.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/modes
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/modes/mode-taxi.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/customer.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components/state.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components/fare.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components/state-log.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components/common.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components/travel-mode.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components/units-geo.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components/units.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components/address.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/components/terms.json src
+./converter.ts node_modules/\@maasglobal/maas-schemas/schemas/core/booking.json src
